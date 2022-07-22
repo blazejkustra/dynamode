8c31d73eb18e63a6298050f455d6e2af1a124e47
Blazej Kustra <blazej.kustra@swmansion.com>
2022-07-22 13:29:31 +0200

    Add basic errors

---
+++ lib/utils/Error.ts
@@ -0,0 +1,18 @@
+const createError = (defaultMessage: string, errorName: string) =>
+  class CustomError extends Error {
+    name: string;
+    message: string;
+
+    constructor(message?: string) {
+      super();
+      this.name = errorName;
+      this.message = message || defaultMessage;
+      Object.setPrototypeOf(this, CustomError.prototype);
+      return this;
+    }
+  };
+
+export const DefaultError = createError('Unexpected Error', 'DefaultError');
+export const InvalidParameter = createError('Invalid Parameter', 'InvalidParameter');
+export const ValidationError = createError('Validation failed', 'ValidationError');
+export const NotFoundError = createError('Item not found', 'NotFoundError');
---
+++ lib/utils/index.ts
@@ -0,0 +1,3 @@
+import * as error from './Error';
+
+export { error };
