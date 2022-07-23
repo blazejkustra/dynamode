8b59a3a6f9ab5e905de68b80d618e39149ec2dba
Blazej Kustra <blazej.kustra@swmansion.com>
2022-07-23 09:02:55 +0200

    Fix tsconfig

--- lib/dynamode.ts
+++ lib/dynamode.ts
@@ -5,3 +5,12 @@ import { Model } from './Model';
 const aws = AWS.default;
 
 export { aws, Table, Model };
+
+///// --- https://github.com/aws/aws-sdk-js-v3/issues/2125 ---
+// some @aws-sdk clients references these DOM lib interfaces,
+// so we need them to exist to compile without having DOM.
+declare global {
+  /* eslint-disable @typescript-eslint/no-empty-interface */
+  interface ReadableStream {}
+  interface File {}
+}
