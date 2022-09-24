2504aaa58613daec37cb04f0bb35560fa0ec4dfa
Blazej Kustra <blazej.kustra@swmansion.com>
2022-09-24 17:48:04 +0200

    Add helpers functions

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -5,3 +5,38 @@ export function checkDuplicatesInArray(arr: Array<unknown>): boolean {
 export function isEmpty(obj: object) {
   return Object.keys(obj).length === 0;
 }
+
+export function isNotEmpty(obj?: object): obj is object {
+  if (!obj) return false;
+  return !isEmpty(obj);
+}
+
+export function isNotEmptyArray<T>(array?: Array<T>): array is Array<T> {
+  return !!array && array.length > 0;
+}
+
+/**
+ * Performs a deep merge of objects and returns new object. Does not modify
+ * objects (immutable) and merges arrays via concatenation.
+ *
+ * @param {...object} objects - Objects to merge
+ * @returns {object} New object with merged key/values
+ */
+export function mergeObjects<T extends Record<string, unknown>>(...objects: T[]): T {
+  const isObject = (obj: unknown): obj is T => !!obj && typeof obj === 'object' && !Array.isArray(obj);
+
+  return objects.reduce((prev, obj) => {
+    Object.keys(obj).forEach((key: keyof T) => {
+      const prevValue = prev[key];
+      const value = obj[key];
+
+      if (isObject(prevValue) && isObject(value)) {
+        prev[key] = mergeObjects(prevValue, value);
+      } else {
+        prev[key] = value;
+      }
+    });
+
+    return prev;
+  }, {} as T);
+}
