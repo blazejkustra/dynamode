d314e4069d23ede9a3df12e397e0915b030dda9a
Blazej Kustra <blazej.kustra@swmansion.com>
2022-09-24 17:45:49 +0200

    Add UnknownClass type

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,7 +1,10 @@
+import { Class } from 'type-fest';
+
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 
 export type AttributeMap = Record<string, AttributeValue>;
 export type GenericObject = Record<string, unknown>;
+export type UnknownClass = Class<unknown>;
 
 type Entry = { key: string; value: unknown };
 
