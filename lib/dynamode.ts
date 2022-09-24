9bcb7ac15f802c7178eb5f447095e524303ce7ad
Blazej Kustra <blazej.kustra@swmansion.com>
2022-09-24 17:50:30 +0200

    Improve imports

--- lib/dynamode.ts
+++ lib/dynamode.ts
@@ -1,6 +1,11 @@
-import { AWS } from '@lib/aws';
+import { Condition } from '@lib/Condition';
+import { column, createdAt, gsiPartitionKey, gsiSortKey, lsiSortKey, prefix, primaryPartitionKey, primarySortKey, suffix, updatedAt } from '@lib/decorators';
 import { Entity } from '@lib/Entity';
+import { Query } from '@lib/Query';
+import { Scan } from '@lib/Scan';
+import { Settings } from '@lib/Settings';
 import { Table } from '@lib/Table';
+import { Transaction } from '@lib/Transaction';
 
 ///// --- https://github.com/aws/aws-sdk-js-v3/issues/2125 ---
 // some @aws-sdk clients references these DOM lib interfaces,
@@ -11,14 +16,39 @@ declare global {
   interface File {}
 }
 
-const aws = AWS.default;
+const settings = Settings.default;
 
 export {
-  //AWS
-  aws,
+  //Condition
+  Condition,
+
+  //decorators
+  column,
+  prefix,
+  suffix,
+  primaryPartitionKey,
+  primarySortKey,
+  gsiPartitionKey,
+  gsiSortKey,
+  lsiSortKey,
+  createdAt,
+  updatedAt,
+
+  //Entity
+  Entity,
+
+  //Query
+  Query,
+
+  //Scan
+  Scan,
+
+  //Settings
+  settings,
+
   //Table
   Table,
-  // Entity
-  Entity,
-  // decorators
+
+  //Transaction
+  Transaction,
 };
