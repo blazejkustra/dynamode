8e2888a7349c120cdb12a8e9a724bc68bac27238
Blazej Kustra <blazej.kustra@swmansion.com>
2022-09-27 20:31:39 +0200

    Remove UnknownClass type

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,10 +1,7 @@
-import { Class } from 'type-fest';
-
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 
 export type AttributeMap = Record<string, AttributeValue>;
 export type GenericObject = Record<string, unknown>;
-export type UnknownClass = Class<unknown>;
 
 type Entry = { key: string; value: unknown };
 
