d9a20e95a62b19ee7af001642125219c29c3112e
Blazej Kustra <blazej.kustra@swmansion.com>
2023-05-31 19:11:40 +0200

    Increase coverage limits and fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  optimizeDeps: {
-    disabled: false, // https://github.com/vitejs/vite/issues/9703
-  },
   build: {
     commonjsOptions: { include: [] },
   },
@@ -17,10 +14,10 @@ export default defineConfig({
       include: ['lib/**'],
       exclude: ['lib/dynamode.ts', 'lib/index.ts', '**/types.ts'],
       all: true,
-      lines: 0,
-      functions: 0,
-      branches: 0,
-      statements: 0,
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
     },
   },
 });
