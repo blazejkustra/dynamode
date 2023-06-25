bc5b4abef0c56f4c8ce5b0fcab5265f2e50ae729
Blazej Kustra <blazej.kustra@swmansion.com>
2023-06-25 16:09:09 +0200

    Remove todos from introduction in docs

--- lib/entity/index.ts
+++ lib/entity/index.ts
@@ -2,7 +2,7 @@ import Dynamode from '@lib/dynamode/index';
 import { DYNAMODE_ENTITY } from '@lib/utils';
 
 export default class Entity {
-  public readonly dynamodeEntity: string;
+  public readonly dynamodeEntity!: string;
 
   // eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
   constructor(...args: unknown[]) {}
