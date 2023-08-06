8d13fe571154d1287d2e64f501588b84e63595ac
Blazej Kustra <blazej.kustra@swmansion.com>
2023-08-06 19:01:22 +0200

    Name arguments better

--- lib/table/helpers/utils.ts
+++ lib/table/helpers/utils.ts
@@ -1,20 +1,20 @@
 import { AttributesMetadata } from '@lib/dynamode/storage/types';
 import { ConflictError, deepEqual, DYNAMODE_DYNAMO_KEY_TYPE_MAP, ValidationError } from '@lib/utils';
 
-export function compareDynamodeEntityWithDynamoTable<T>(aa: T[], bb: T[]): void {
-  aa.forEach((a) => {
-    if (!bb?.some((b) => deepEqual(a, b))) {
+export function compareDynamodeEntityWithDynamoTable<T>(dynamodeSchema: T[], ddbSchema: T[]): void {
+  dynamodeSchema.forEach((a) => {
+    if (!ddbSchema?.some((b) => deepEqual(a, b))) {
       throw new ConflictError(`Key "${JSON.stringify(a)}" not found in table`);
     }
   });
 
-  bb.forEach((a) => {
-    if (!aa.some((b) => deepEqual(a, b))) {
+  ddbSchema.forEach((a) => {
+    if (!dynamodeSchema.some((b) => deepEqual(a, b))) {
       throw new ConflictError(`Key "${JSON.stringify(a)}" not found in entity`);
     }
   });
 
-  if (aa.length !== bb.length) {
+  if (dynamodeSchema.length !== ddbSchema.length) {
     throw new ConflictError('Key schema length mismatch between table and entity');
   }
 }
