818f5c5c8222df9a90785d6d156b517044468f8f
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-18 17:31:26 +0100

    WIP: DynamoDB stream support

--- examples/User/model.ts
+++ examples/User/model.ts
@@ -71,3 +71,20 @@ async function create() {
   const table = await UserTableManager.createTable();
   console.log(table);
 }
+
+type AttributeValue = any;
+export type StreamRecord = {
+  Keys?: Record<string, AttributeValue> | undefined;
+  NewImage?: Record<string, AttributeValue> | undefined;
+  OldImage?: Record<string, AttributeValue> | undefined;
+  StreamViewType?: 'KEYS_ONLY' | 'NEW_IMAGE' | 'OLD_IMAGE' | 'NEW_AND_OLD_IMAGES' | undefined;
+};
+
+export type DynamoDBRecord = {
+  eventName?: 'INSERT' | 'MODIFY' | 'REMOVE' | undefined;
+  dynamodb?: StreamRecord | undefined;
+};
+
+export type DynamoDBStreamEvent = {
+  Records?: DynamoDBRecord[] | undefined;
+};
