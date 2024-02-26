4a1f0da785d3686b54f3b5a821f3a053a38a2332
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-26 23:19:45 +0100

    Fix stream types for aws-lambda

--- lib/stream/types.ts
+++ lib/stream/types.ts
@@ -1,8 +1,22 @@
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/aws-lambda/trigger/dynamodb-stream.d.ts
+type AttributeValueAWSLambda = {
+  B?: string | undefined;
+  BS?: string[] | undefined;
+  BOOL?: boolean | undefined;
+  L?: AttributeValueAWSLambda[] | undefined;
+  M?: { [id: string]: AttributeValueAWSLambda } | undefined;
+  N?: string | undefined;
+  NS?: string[] | undefined;
+  NULL?: boolean | undefined;
+  S?: string | undefined;
+  SS?: string[] | undefined;
+};
+
 export type StreamPayload = {
-  NewImage?: Record<string, AttributeValue> | undefined;
-  OldImage?: Record<string, AttributeValue> | undefined;
+  NewImage?: Record<string, AttributeValue | AttributeValueAWSLambda> | undefined;
+  OldImage?: Record<string, AttributeValue | AttributeValueAWSLambda> | undefined;
   StreamViewType?: 'KEYS_ONLY' | 'NEW_IMAGE' | 'OLD_IMAGE' | 'NEW_AND_OLD_IMAGES' | undefined;
 };
 
